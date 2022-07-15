ec1290b4446a778f87894858b9f298273238b89c
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-07-15 16:55:36 +0430

    Add Repository

--- WOW/GlobalUsings.cs
+++ WOW/GlobalUsings.cs
@@ -1,1 +1,3 @@
 ﻿global using WOW.Base;
+global using WOW.Models;
+global using WOW.Repositories;
---
+++ WOW/Repositories/PersonRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace WOW.Repositories
+{
+    public class PersonRepository
+    {
+        private List<Person> persons = new();
+
+        public bool Add(Person model)
+        {
+            try
+            {
+                persons.Add(model);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Person model)
+        {
+            try
+            {
+                persons.Remove(model);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Update(Person model)
+        {
+            try
+            {
+                Person? oldModel = persons.SingleOrDefault(x => x.Id == model.Id);
+                if (oldModel != null)
+                {
+                    persons.Remove(oldModel);
+                    persons.Add(model);
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public List<Person> Get() => persons;
+    }
+}
